2a9943d150f6a1a0a388f429d93e24dd4898b6d2
Kabeer Ali <100375085+kabeerali1825@users.noreply.github.com>
2023-10-04 07:21:40 -0700

    Assignment2-Bitf20m018.cs

--- Assignment2-bitf20m018.cs
+++ Assignment2-bitf20m018.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -129,31 +129,22 @@ namespace Assignment_2
 
             return unboxedValue;
         }
-        static void Main(string[] args)
+        static void Task05()
         {
-
-            //Task-01
-            Console.WriteLine("Please enter your first name:");
-            string fName = Console.ReadLine();
-            Console.WriteLine("Please enter your last name:");
-            string lName = Console.ReadLine();
-            string fullName = ConcatenateNames(fName, lName);
-            Console.WriteLine("Your full name is: " + fullName);
+            //Task-05
+            //a
+            string[] fruits = { "Apple", "Banana", "Orange", "Grapes", "Strawberry" };
+            Console.WriteLine("Using a for loop:");
+            ArrayIterationForEachLoop(fruits);
             Console.WriteLine();
-
-            //Task-02
-            Console.WriteLine("Please enter a sentence to find substring:");
-            string sentence = Console.ReadLine();
-            string lastFiveCharacters = FetchingSubstring(sentence);
-            Console.WriteLine("Last 5 characters: " + lastFiveCharacters);
+            //b
+            string[] colors = { "Red", "Blue", "Green", "Yellow", "Orange" };
+            Console.WriteLine("Using a foreach loop:");
+            ArrayIterationForEach(colors);
             Console.WriteLine();
-
-
-            //Task-03
-            string message = GetTemperatureMessage();
-            Console.WriteLine(message);
-            Console.WriteLine();
-
+        }
+        static void Task04()
+        {
             //Task-04
             int[] numbers = new int[5];
             numbers[0] = 1;
@@ -167,71 +158,9 @@ namespace Assignment_2
                 Console.WriteLine(number);
             }
             Console.WriteLine();
-
-            //Task-05
-            //a
-            string[] fruits = { "Apple", "Banana", "Orange", "Grapes", "Strawberry" };
-            Console.WriteLine("Using a for loop:");
-            ArrayIterationForEachLoop(fruits);
-            Console.WriteLine();
-            //b
-            string[] colors = { "Red", "Blue", "Green", "Yellow", "Orange" };
-            Console.WriteLine("Using a foreach loop:");
-            ArrayIterationForEach(colors);
-            Console.WriteLine();
-
-
-
-
-            //Task-06
-
-            int[] scores = new int[10];
-            Random random = new Random();
-            for (int i = 0; i < scores.Length; i++)
-            {
-                scores[i] = random.Next(30, 100);
-            }
-            int sum = CalculateSum(scores);
-            Console.WriteLine("Test Scores are:");
-            foreach (int score in scores)
-            {
-                Console.Write(score + " ");
-            }
-            Console.WriteLine("\nTotal Sum: " + sum);
-            Console.WriteLine();
-
-
-            //Task-07
-            int[] values = { 34, 56, 12, 78, 45, 90, 23, 67, 89, 55 };
-            int maxValue = ToFindMaxValue(values);
-            Console.WriteLine("The maximum value in the array is: " + maxValue);
-            Console.WriteLine();
-
-            //Task-08
-            int[] numbers1= { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            ReverseTheArray(numbers1);
-
-            Console.WriteLine("Reversed Array:");
-            foreach (int numb in numbers1)
-            {
-                Console.Write(numb + " ");
-            }
-            Console.WriteLine();
-
-            //Task-9
-            // a.Boxing and Unboxing an Integer:
-            int x = 72;
-            int y = BoxUnboxAnInteger(x);
-            Console.WriteLine("Original integer (x): " + x);
-            Console.WriteLine("Unboxed integer (y): " + y);
-            Console.WriteLine();
-            // a.Boxing and Unboxing a Double:
-            double doubleVal = 3.174159;
-            double unboxedVal = BoxUnboxADouble(doubleVal);
-            Console.WriteLine("Original double (doubleValue): " + doubleVal);
-            Console.WriteLine("Unboxed double (unboxedValue): " + unboxedVal);
-            Console.WriteLine();
-
+        }
+        static void Task10()
+        {
             //TASK-10
             //a.Box and Unbox in an Array of Integers:
 
@@ -267,10 +196,10 @@ namespace Assignment_2
                 Console.WriteLine($"Element: {item}, Type: {item.GetType()}");
             }
             Console.WriteLine();
-
-
+        }
+        static void Task11()
+        {
             //Task-11
-
             //a. Using the dynamic keyword to assign different types to a variable:
             dynamic myVariable;
             myVariable = 42;
@@ -290,6 +219,186 @@ namespace Assignment_2
             Console.WriteLine("myVariable2 (DateTime) Type: " + myVariable2.GetType());
             myVariable2 = "Dynamic";
             Console.WriteLine("myVariable2 (string) Type: " + myVariable2.GetType());
+        }
+        //static void Main(string[] args)
+        //{
+
+
+        
+
+
+        //    //Task-07
+        
+
+        
+
+        
+
+
+
+
+        //}
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose a task to perform:");
+                Console.WriteLine("1. String Concatenation");
+                Console.WriteLine("2. Substring Fetching");
+                Console.WriteLine("3. String Interpolation");
+                Console.WriteLine("4. Array Declaration and Initialization");
+                Console.WriteLine("5. Array Iteration (For Loop) and (Foreach Loop)");
+                Console.WriteLine("6. Sum of Array Elements");
+                Console.WriteLine("7. Finding Maximum Value in an Array");
+                Console.WriteLine("8. Array Reversal");
+                Console.WriteLine("9. Boxing and Unboxing");
+                Console.WriteLine("10. Dynamic Variables");
+                Console.WriteLine("0. Exit");
+
+                int choice;
+                Console.WriteLine("Please Enter Your Choice: ");
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            // Task 01 - String Concatenation
+                            Console.WriteLine("String Concatenation Task:");
+                            Console.WriteLine("Please enter your first name:");
+                            string fName = Console.ReadLine();
+                            Console.WriteLine("Please enter your last name:");
+                            string lName = Console.ReadLine();
+                            string fullName = ConcatenateNames(fName, lName);
+                            Console.WriteLine("Your full name is: " + fullName);
+                            Console.WriteLine();
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            // Task 02 - Substring Fetching
+                            Console.WriteLine("Substring Fetching Task:");
+                            Console.WriteLine("Please enter a sentence to find substring:");
+                            string sentence = Console.ReadLine();
+                            string lastFiveCharacters = FetchingSubstring(sentence);
+                            Console.WriteLine("Last 5 characters: " + lastFiveCharacters);
+                            Console.WriteLine();
+                            break;
+
+                        case 3:
+                            Console.Clear();
+                            // Task 03 - String Interpolation
+                            Console.WriteLine("String Interpolation Task:");
+                            string message = GetTemperatureMessage();
+                            Console.WriteLine(message);
+                            Console.WriteLine();
+                            break;
+
+                        case 4:
+                            Console.Clear();
+                            // Task 04 - Array Declaration and Initialization
+                            Console.WriteLine("Array Declaration and Initialization Task:");
+                            Task04();
+                            Console.WriteLine();
+                            break;
+
+                        case 5:
+                            Console.Clear();
+                            // Task 05 - Array Iteration (For Loop)
+                            Console.WriteLine("Array Iteration (For Loop) and (Foreach Loop)Task:");
+                            Task05();
+                            Console.WriteLine();
+                            break;
+
+                        case 6:
+                            Console.Clear();
+                            // Task 06 - Sum of Array Elements
+                            Console.WriteLine("Sum of Array Elements Task:");
+                            int[] scores = new int[10];
+                            Random random = new Random();
+                            for (int i = 0; i < scores.Length; i++)
+                            {
+                                scores[i] = random.Next(30, 100);
+                            }
+                            int sum = CalculateSum(scores);
+                            Console.WriteLine("Test Scores are:");
+                            foreach (int score in scores)
+                            {
+                                Console.Write(score + " ");
+                            }
+                            Console.WriteLine("\nTotal Sum: " + sum);
+                            Console.WriteLine();
+                            // Implement Task 07 here
+                            Console.WriteLine();
+                            break;
+
+                        case 7:
+                            Console.Clear();
+                            // Task 08 - Finding Maximum Value in an Array
+                            Console.WriteLine("Finding Maximum Value in an Array Task:");
+                            int[] values = { 34, 56, 12, 78, 45, 90, 23, 67, 89, 55 };
+                            int maxValue = ToFindMaxValue(values);
+                            Console.WriteLine("The maximum value in the array is: " + maxValue);
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            break;
+
+
+                        case 8:
+                            Console.Clear();
+                            Console.WriteLine("Array Reversal Task:");
+                            //Task-08
+                            int[] numbers1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                            ReverseTheArray(numbers1);
+
+                            Console.WriteLine("Reversed Array:");
+                            foreach (int numb in numbers1)
+                            {
+                                Console.Write(numb + " ");
+                            }
+                            Console.WriteLine();
+                            // Implement Task 09 here
+                            Console.WriteLine();
+                            break;
+
+
+                        case 9:
+                            Console.Clear();
+                            // Task 10 - Boxing and Unboxing
+                            Console.WriteLine("Boxing and Unboxing Task:");
+                            Task10();
+                            Console.WriteLine();
+                            break;
+
+
+                        case 10:
+                            Console.Clear();
+                            // Task 11 - Dynamic Variables
+                            Console.WriteLine("Dynamic Variables Task:");
+                            Task11();
+                            Console.WriteLine();
+                            break;
+
+
+                    
+
+                        case 0:
+                            // Exit the program
+                            return;
+
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Invalid choice. Please choose a valid task.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
         }
     }
 }
